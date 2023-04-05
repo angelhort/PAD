2a14cad411ed797783c8a3ea771a0fc825adec2a
Pablovald <pablovaldesbalduz@gmail.com>
2023-04-05 12:28:42 +0200

    Cambios para cómo jugar
    Terminados los layouts e implementadas las clases para las slides, queda testear que salga todo bien

--- Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HowToPlayActivity.java
+++ Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HowToPlayActivity.java
@@ -1,14 +1,64 @@
 package es.ucm.fdi.sacapalabra;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class HowToPlayActivity extends AppCompatActivity {
+public class HowToPlayActivity extends FragmentActivity {
+
+    private static final int NUM_PAG = 5;
+    private ViewPager page;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        page = (ViewPager) findViewById(R.id.view_pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        page.setAdapter(pagerAdapter);
     }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(position == 0){
+                return new HowToPlayFragment1();
+            }
+
+            else if (position == 1){
+                return new HowToPlayFragment2();
+            }
+
+            else if (position == 2){
+                return new HowToPlayFragment3();
+            }
+
+            else if (position == 3){
+                return new HowToPlayFragment4();
+            }
+
+            else if (position == 4){
+                return new HowToPlayFragment5();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+
 }
---
+++ Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HowToPlayFragment4.java
@@ -0,0 +1,21 @@
+package es.ucm.fdi.sacapalabra;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class HowToPlayFragment4 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.activity_how_to_play_fragment_4, container, false);
+
+        return rootView;
+    }
+
+}
