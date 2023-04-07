3cfb8309385048718ee919f50d98a4d195cb6d0c
gavilaneees <davidgd2001@hotmail.es>
2023-04-07 18:05:43 +0200

    Esqueleto Historial

--- Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HistoryActivity.java
+++ Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HistoryActivity.java
@@ -3,6 +3,8 @@ package es.ucm.fdi.sacapalabra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        ListView historial = findViewById(R.id.lvHistorialPartidas);
+        TextView textRachaVictorias = findViewById(R.id.textRachaVictorias);
+        TextView textMejorRachaVictorias = findViewById(R.id.textMejorRachaVictorias);
+        TextView textPartidas = findViewById(R.id.textPartidas);
+        TextView textVictoriasPorcentaje = findViewById(R.id.textVictoriasPorcentaje);
     }
 }
