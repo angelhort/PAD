c58aa0041b5fe9b7a670f022f4e2e55125b786b2
angelhort <75445093+angelhort@users.noreply.github.com>
2023-04-27 12:27:50 +0200

    Historial puesto, faltan colores

--- Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HistoryActivity.java
+++ Proyecto/app/src/main/java/es/ucm/fdi/sacapalabra/HistoryActivity.java
@@ -1,7 +1,6 @@
 package es.ucm.fdi.sacapalabra;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
@@ -9,11 +8,16 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class HistoryActivity extends BaseActivity {
@@ -74,9 +78,7 @@ public class HistoryActivity extends BaseActivity {
 
     private void displayGameHistory() {
         Cursor cursor = dbHelper.getAllGames();
-        ArrayList<String> lista = new ArrayList<String>();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista);
-        historial.setAdapter(adapter);
+        ArrayList<MyData> lista = new ArrayList<MyData>();
 
         // Iterate through the cursor to get the game data
         while (cursor.moveToNext()) {
@@ -85,10 +87,17 @@ public class HistoryActivity extends BaseActivity {
             String word = cursor.getString(cursor.getColumnIndexOrThrow(GameContract.GameEntry.COLUMN_NAME_WORD));
             int tries = cursor.getInt(cursor.getColumnIndexOrThrow(GameContract.GameEntry.COLUMN_NAME_TRIES));
             int result = cursor.getInt(cursor.getColumnIndexOrThrow(GameContract.GameEntry.COLUMN_NAME_RESULT));
-            Log.d("palabra", word);
-            lista.add(word);
+
+            language = (language.substring(0, 1).toUpperCase() + language.substring(1)).trim();
+            mode = (mode.substring(0, 1).toUpperCase() + mode.substring(1)).trim();
+            word = (word.substring(0, 1).toUpperCase() + word.substring(1)).trim();
+           
+            lista.add(new MyData(language, mode, word, tries));
+            Log.d("PalabraHistorial", word);
         }
         cursor.close();
+        HistoryAdapter adapter = new HistoryAdapter(this, lista);
+        historial.setAdapter(adapter);
         adapter.notifyDataSetChanged();
     }
     private void setTheme(String theme){
@@ -106,5 +115,77 @@ public class HistoryActivity extends BaseActivity {
         Configuration conf = res.getConfiguration();
         conf.locale = locale;
         res.updateConfiguration(conf, dm);
+    }
+    public class HistoryAdapter extends BaseAdapter {
+        private List<MyData> mDataList;
+        private LayoutInflater mInflater;
+
+        public HistoryAdapter(Context context, List<MyData> dataList) {
+            mDataList = dataList;
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mDataList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDataList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.activity_history_items, null);
+
+                holder = new ViewHolder();
+                holder.languageTextView = convertView.findViewById(R.id.textViewIdioma);
+                holder.modeTextView = convertView.findViewById(R.id.textViewModo);
+                holder.wordTextView = convertView.findViewById(R.id.textViewPalabra);
+                holder.triesTextView = convertView.findViewById(R.id.textViewIntentos);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            MyData data = mDataList.get(position);
+
+            holder.languageTextView.setText(data.language);
+            holder.modeTextView.setText(data.mode);
+            holder.wordTextView.setText(data.word);
+            holder.triesTextView.setText(String.valueOf(data.tries));
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            TextView languageTextView;
+            TextView modeTextView;
+            TextView wordTextView;
+            TextView triesTextView;
+        }
+    }
+    public class MyData {
+        public String language;
+        public String mode;
+        public String word;
+        public int tries;
+
+        public MyData(String language, String mode, String word, int tries) {
+            this.language = language;
+            this.mode = mode;
+            this.word = word;
+            this.tries = tries;
+        }
     }
 }
